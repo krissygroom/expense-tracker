3f85e93a572b5a03faf8997192990a554700f3a8
krissy <krissydgroom@gmail.com>
2022-03-04 17:11:49 -0800

    Add first custom expense item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 10, 12),
+    },
+  ];
+
   return (
     <div>
-      <h2>Yo Yo Yo!</h2>
+      <h2 className="title">Expense Tracker</h2>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          date={expense.date}
+          title={expense.title}
+          amount={expense.amount}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+const ExpenseItem = ({ date, title, amount }) => {
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth()).padStart(2, "0");
+  const yyyy = date.getFullYear();
+  return (
+    <div className="expense-item">
+      <div>{mm + "/" + dd + "/" + yyyy}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{`$${amount}`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
