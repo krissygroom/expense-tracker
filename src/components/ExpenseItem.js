eca5811bde1315820a316de3961be750e0f6eb4d
krissy <krissydgroom@gmail.com>
2022-03-11 18:22:24 -0800

    Small style edits

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,7 +9,7 @@ const ExpenseItem = ({ date, title, amount }) => {
     <div className="expense-item">
       <div className="expense-item__date">{mm + "/" + dd + "/" + yyyy}</div>
       <div className="expense-item__description">
-        <h2> - {title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{`$${amount}`}</div>
       </div>
     </div>
