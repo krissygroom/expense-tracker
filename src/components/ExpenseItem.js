a3cc5adf75334a3610202a1755f96589b427b8bd
krissy <krissydgroom@gmail.com>
2022-03-11 18:11:01 -0800

    Small color edits

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,9 +7,9 @@ const ExpenseItem = ({ date, title, amount }) => {
   const yyyy = date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{mm + "/" + dd + "/" + yyyy}</div>
+      <div className="expense-item__date">{mm + "/" + dd + "/" + yyyy}</div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2> - {title}</h2>
         <div className="expense-item__price">{`$${amount}`}</div>
       </div>
     </div>
