6d28d6c91243c94b8c852ddfd65c7a1f4719b6db
krissy <krissydgroom@gmail.com>
2022-03-12 13:54:29 -0800

    Added ExpenseDate component and styling

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate({ date }) {
+  const dd = date.toLocaleString("en-US", { day: "2-digit" });
+  const mm = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-item__date">
+      <div className="expense-item__date-month">{mm}</div>
+      <div className="expense-item__date-year">{year}</div>
+      <div className="expense-item__date-day">{dd}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
-  const dd = String(date.getDate()).padStart(2, "0");
-  const mm = String(date.getMonth()).padStart(2, "0");
-  const yyyy = date.getFullYear();
   return (
     <div className="expense-item">
-      <div className="expense-item__date">{mm + "/" + dd + "/" + yyyy}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{`$${amount}`}</div>
